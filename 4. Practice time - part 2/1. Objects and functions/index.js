27c2e684a3d6b3dd7afccd82c4c933abb6036679
Megan Thong <meganthong@outlook.com>
2021-07-14 22:23:59 +0800

    2nd practice

--- 4. Practice time - part 2/1. Objects and functions/index.js
+++ 4. Practice time - part 2/1. Objects and functions/index.js
@@ -6,3 +6,14 @@
 // "Per is 35 years old and lives in Norway"
 
 // Call the logData() function to verify that it works
+let person = {
+    name: "Lawrence",
+    age: 19,
+    country: "Scotland"
+}
+
+function logData() {
+    console.log(person.name + " is " + person.age + " years old and lives in " + person.country)
+}
+
+logData()
